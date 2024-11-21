53143cd88e666cfaf62eb0b6960734b8d29074fb
mjerta <maarten.postma1000@gmail.com>
2024-11-21 23:53:43 +0100

    Add a paragraph

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,27 @@
 function Home() {
   return (
-    <h1>
-      another test!!!
-    </h1>
+    <>
+      <h1>
+        another test!!!
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+         dicta eligendi id ipsum perferendis possimus? Aliquid amet, aspernatur
+         atque, cumque debitis delectus ea eaque eius ex ipsam laudantium
+         maiores modi non odio optio perspiciatis, porro quos reprehenderit
+         saepe sed sunt veniam voluptates. Adipisci eius in, laboriosam nostrum
+         placeat quo velit! Commodi consequatur culpa ipsum iste officiis, porro
+         quam saepe sint! Aliquam aut eveniet explicabo obcaecati tenetur. A
+         amet asperiores deserunt eligendi facilis harum in ipsa, ipsam, maxime
+         natus omnis possimus quis saepe, sapiente soluta tenetur voluptate?
+         Aspernatur assumenda at corporis cumque debitis dicta ex hic nostrum
+         optio possimus quasi quibusdam, totam, ullam? A aliquam animi autem
+         consequuntur cum deleniti dignissimos dolore ducimus earum eos error
+         esse exercitationem illo ipsum laborum, minus molestias nulla odit
+         pariatur quae quasi quis repellat tempore vitae voluptatem! Architecto
+         at cupiditate delectus dolorem enim est ex harum hic, illum iure labore
+         laboriosam natus obcaecati, rem temporibus?</p>
+    </>
   )
 }
+
 export default Home;
